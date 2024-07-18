a01da7801a7dea7224e5644bce59371ad923e0c7
shouvik sinha <shouviksinha901@gmail.com>
2024-07-19 02:38:25 +0530

    change in input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import useState from 'react';
+import { useState } from "react";
 
 const [userInput, setUserInput] = useState({
   initialInvestment:  10000,
@@ -22,21 +22,37 @@ export default function Input(){
            <div className="input-group">
             <p>
              <label>Initial Investment</label>
-             <input type="number" required />
+             <input type="number" 
+             required
+             value = {userInput.initialInvestment}
+             onChange={(event) =>handleInput('initialInvestment',event.target.value)}
+             />
              </p>
              <p>
               <label>Annual Investment</label>
-              <input type="number" required />
+              <input type="number" 
+               required
+               value = {userInput.annualInvestment}
+               onChange={(event) =>handleInput('annualInvestment',event.target.value)}
+             />
               </p>
           </div>
           <div class="input-group">
             <p>
               <label>Expected Result</label>
-              <input type="number" required />
+              <input type="number" 
+               required
+               value={userInput.expectedReturn}
+               onChange={(event) =>handleInput('expectedReturn',event.target.value)}
+             />
             </p>
             <p>
               <label>Duration</label>
-              <input type="number" required />
+              <input type="number" 
+               required
+               value={userInput.Duration}
+               onChange={(event) =>handleInput('Duration',event.target.value)}
+             />
             </p>
           </div>
       </section>
