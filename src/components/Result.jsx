688e0226e37340ccf75260f1354145d7ad030dee
shouvik sinha <shouviksinha901@gmail.com>
2024-07-20 23:52:40 +0530

    Outputing result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,6 +1,7 @@
 import {calculateInvestmentResults, formatter} from "../util/Investment"
 export default function Result({input}){
     const resultData = calculateInvestmentResults(input);
+    const initialInvestment = resultData[0].valueEndOfYear - resultData[0].interest - resultData[0].annualInvestment;
     return(
         <table id="result">
             <thead>
@@ -14,12 +15,13 @@ export default function Result({input}){
             </thead>
             <tbody>
                 {resultData.map(yearData => {
+                    const totalInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year;
                     return <tr key={yearData.year}>
                         <td>{yearData.year}</td>
                         <td>{formatter.format(yearData.valueEndOfYear)}</td>
                         <td>{formatter.format(yearData.interest)}</td>
-                        <td></td>
-                        <td></td>
+                        <td>{formatter.format(totalInterest)}</td>
+                        <td>{formatter.format(initialInvestment)}</td>
                         
                     </tr>
                 })}
